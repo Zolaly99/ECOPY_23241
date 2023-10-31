50de4752f1a9f1bb60aa390bbe5af68339d69619
Zolaly99 <zolip99@gmail.com>
2023-10-31 19:40:08 +0100

    6. héz th update ZALLWZ

--- src/linear_regression/LinearRegressions.py
+++ src/linear_regression/LinearRegressions.py
@@ -1,22 +1,34 @@
 import pandas as pd
-#import statsmodels
-#from statsmodels.api import OLS
-
-
 import statsmodels.api as sm
-class LinearRegressionSM():
 
-    def __init__(self, df1, df2):
-        self.left_hand_side = df1
-        self.right_hand_side = df2
+
+class LinearRegressionSM:
+    def __init__(self, left_hand_side, right_hand_side):
+        self.left_hand_side = left_hand_side
+        self.right_hand_side = right_hand_side
         self._model = None
 
     def fit(self):
-        right_df = sm.add_constant(self.right_hand_side)
-        model = sm.OLS(self.left_hand_side, right_df).fit()
-        self._model = model
+        right_hand_side = sm.add_constant(self.right_hand_side)
+        _model = sm.OLS(self.left_hand_side, right_hand_side).fit()
+        self._model = _model
 
     def get_params(self):
-        beta_coeffs = self._model.params
-        beta_coeffs.name = 'Beta coefficients'
-        return beta_coeffs
+        params = self._model.params
+        return pd.Series(params, name='Beta coefficients')
+
+    def get_pvalues(self):
+        p_values = self._model.pvalues
+        return pd.Series(p_values, name='P-values for the corresponding coefficients')
+
+    def get_wald_test_result(self, restr_matrix):
+        wald = self._model.wald_test(restr_matrix)
+        f_stat = float(wald.statistic)
+        p_val = float(wald.pvalue)
+        return f'F-value: {f_stat:.3}, p-value: {p_val:.3}'
+
+    def get_model_goodness_values(self):
+        adj_r2 = self._model.rsquared_adj
+        aic = self._model.aic
+        bic = self._model.bic
+        return f'Adjusted R-squared: {adj_r2:.3}, Akaike IC: {aic:.3}, Bayes IC: {bic:.3}'
--- src/weekly/weekly_test_6.py
+++ src/weekly/weekly_test_6.py
@@ -1,23 +1,16 @@
+import pandas as pd
+import statsmodels
+
+
 sp500 = pd.read_parquet("C:/Users/MSI laptop/Downloads/sp500.parquet", engine = "fastparquet")
-
-ff_factors = pd.read_parquet("C:/Users/MSI laptop/Downloads/ff_factors.parquet", engine = "fastparquet")
-
+ff_factors = pd.read_parquet("C:/Users/MSI laptop/Downloads/ff_factors.parquet")
 df_new = sp500.merge(ff_factors, on='Date', how='left')
-
 df_new["Excess Return"] = df_new["Monthly Returns"]-df_new["RF"]
-
 df_new = df_new.sort_values(by=['Date']).reset_index()
-
+df_new["ex_ret_1"]=df_new.groupby(['Date'])['Excess Return'].shift(1)
 df_new["ex_ret_1"]=df_new.groupby("Symbol")["Excess Return"].shift(1)
-
 df_new = df_new.dropna(subset = ['ex_ret_1'])
-
 df_new = df_new.dropna(subset = ['HML'])
-
 df_amzn = df_new[df_new["Symbol"]=="AMZN"]
-
 df_amzn = df_amzn.drop("Symbol", axis = 1)
-
-left_hand_side = df_amzn[["Excess Return"]]
-
-right_hand_side = df_amzn[["Mkt-RF", "SMB", "HML"]]
+df_amzn.set_index('Date').reset_index()
